fb919722d6cdbac020449142d738388557aca558
Heather Peterson <hpeterson462@gmail.com>
2020-11-04 15:58:34 -0800

    test passing

--- src/reducers/UserReducer.test.js
+++ src/reducers/UserReducer.test.js
@@ -4,7 +4,7 @@ import { setUsers } from '../actions/InputActions';
 describe('user reducer', () => {
   it('handles the SET_USERS action', () => {
     const state = {
-      user: [],
+      users: {},
       error: null
     };
 
@@ -20,13 +20,14 @@ describe('user reducer', () => {
     const newState = reducer(state, action);
 
     expect(newState).toEqual({
-      user: [{ login: 'hpeterson462' },
-      { name: 'Heather Peterson' },
-      { followers_url: 'https://api.github.com/users/hpeterson462/followers' },
-      { following_url: 'https://api.github.com/users/hpeterson462/following{/other_user}' },
-      { html_url: 'https://github.com/hpeterson462' },
-      { repos_url: 'https://api.github.com/users/hpeterson462/repos' }
-      ],
+      users: {
+        login: 'hpeterson462',
+        name: 'Heather Peterson',
+        followers_url: 'https://api.github.com/users/hpeterson462/followers',
+        following_url: 'https://api.github.com/users/hpeterson462/following{/other_user}',
+        html_url: 'https://github.com/hpeterson462',
+        repos_url: 'https://api.github.com/users/hpeterson462/repos'
+      },
       error: null
     });
   });
